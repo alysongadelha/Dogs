5dc6bfb3e491ae4a51d465cf37ba48c1d5543249
Alyson Gadelha <alyson_sud@hotmail.com>
2022-05-21 22:13:36 -0300

    SAVEPOINT

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
+import Button from "../Form/Button";
+import Input from "../Form/Input";
+import Error from "../Helper/Error";
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Form/Button.module.css'
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubimit(event) {
+    event.preventDefault();
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubimit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+
+        {<Error error={error} />}
+      </form>
+      <Link className={styles.perdeu} to='/login/perdeu'>Perdeu a Senha?</Link>
+      <div className={styles.cadastro}>
+          <h2 className={styles.subtitle}>Cadastre-se</h2>
+          <p>Ainda não possui conta? Cadastre-se no site.</p>
+      <Link className={stylesBtn.button} to="/login/criar">Criar</Link>
+      </div>
+    </section>
+  );
+};
+
+export default LoginForm;
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/,
+    message: "Preencha um email válido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  function validate(values) {
+    if (type === false) return true;
+    if (values.length === 0) {
+      setError("Preencha um valor.");
+      return false;
+    } else if (types[type] && !types[type].regex.test(values)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
